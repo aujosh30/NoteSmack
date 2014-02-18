f6c1a751a59839f1649a2bec41c974dbcdae40ac
Josh Kendrick <jck0004@tigermail.auburn.edu>
2014-02-17 22:31:46 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NoteSmack::Application.config.secret_key_base = 'e6e2f4af37e28e08960e52aa48f7052d3debb6fd2c302a418d936a176d123e863e43b6a1dff588321186fda0b051b65e9dbc3cb8a17b684b5491dcc9e513236c'
